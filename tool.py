fc5871475fd642b6a5eff00aeda087139b09781b
DinoDx <r.rapacciuolo1@studenti.unisa.it>
2023-06-07 16:11:25 +0200

    refactor output

--- tool.py
+++ tool.py
@@ -12,29 +12,27 @@ for i in range(len(meta_dataset)):
 
     temp_smells = smell_detection(meta_dataset['path'][i])
     temp_smells['name'] = meta_dataset['name'][i]
-    smells = smells.append(temp_smells)
-    
-    
-    
-    
-    
-    statisticalParity, predictiveParity, fairnessThroughAwareness = fairness_assessment(path=meta_dataset['path'][i],
-                                                                    label_name=meta_dataset['label_name'][i],
-                                                                    favorable_classes=meta_dataset['favorable_classes'][i],
-                                                                    protected_attribute_names=meta_dataset['protected_attribute_names'][i],
-                                                                    privileged_classes=meta_dataset['privileged_classes'][i],
-                                                                    features_to_drop=meta_dataset['features_to_drop'][i],
-                                                                    privileged_groups=meta_dataset['privileged_groups'][i],
-                                                                    unprivileged_groups=meta_dataset['unprivileged_groups'][i])
+    smells = pd.concat([smells, temp_smells])
 
+    statisticalParity, predictiveParity, fairnessThroughAwareness = fairness_assessment(
+        path=meta_dataset['path'][i],
+        label_name=meta_dataset['label_name'][i],
+        favorable_classes=meta_dataset['favorable_classes'][i],
+        protected_attribute_names=meta_dataset['protected_attribute_names'][i],
+        privileged_classes=meta_dataset['privileged_classes'][i],
+        features_to_drop=meta_dataset['features_to_drop'][i],
+        privileged_groups=meta_dataset['privileged_groups'][i],
+        unprivileged_groups=meta_dataset['unprivileged_groups'][i])
 
-    fairness = fairness.append({'name': meta_dataset['name'][i],
-                                'statisticalParity': statisticalParity,
-                                'predictiveParity': predictiveParity,
-                                'fairnessThroughAwareness': fairnessThroughAwareness.item()}, ignore_index=True)
-    
-    
+    fairness = pd.concat([
+        fairness,
+        pd.DataFrame({'name': [meta_dataset['name'][i]],
+                      'statisticalParity': [statisticalParity],
+                      'predictiveParity': [predictiveParity],
+                      'fairnessThroughAwareness': [fairnessThroughAwareness.item()]})
+    ], ignore_index=True)
 
+    
 print(smells)
 print(fairness)
 
