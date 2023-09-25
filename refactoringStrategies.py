17f4b7634bc47300cac69a8e2f0e0747d23c7591
DinoDx <r.rapacciuolo1@studenti.unisa.it>
2023-09-25 15:33:55 +0200

    add refactored datasets

--- refactoringStrategies.py
+++ refactoringStrategies.py
@@ -7,16 +7,18 @@ datasets = pd.read_csv('input/paths.csv')
 for i in range(len(datasets)):
 
     dataset = pd.read_csv(datasets['path'][i])
+    print(datasets['path'][i])
     datasmells = datasmell[datasmell['name'] == datasets['name'][i]][['attribute','Data Smell Type','Faulty Element Overview']]
 
     for attribute in dataset:
-        if attribute in datasmells["attribute"].values:
+         print(attribute)
+         if attribute in datasmells["attribute"].values:
             smell_type = datasmells[datasmells["attribute"] == attribute]["Data Smell Type"].values[0]
-        
+
             if smell_type == "Extreme Value Smell":
-                values = ast.literal_eval(datasmells['Faulty Element Overview'].values[0])
+                values = ast.literal_eval(datasmells[datasmells["attribute"] == attribute]['Faulty Element Overview'].values[0])
+                max_value = dataset.loc[~dataset[attribute].isin(values)][attribute].max()
                 min_value = dataset.loc[~dataset[attribute].isin(values)][attribute].min()
-                max_value = min(values)-1
                 dataset[attribute] = dataset[attribute].clip(lower=min_value, upper=max_value)
                 #print(attribute, dataset[attribute].values)
 
@@ -29,14 +31,14 @@ for i in range(len(datasets)):
                             element = dataset[attribute].mode()[0]
                         else:
                             dataset.drop(dataset[attribute], axis=1, inplace=True)
-
                         #print(attribute, dataset[attribute].values)
 
             elif smell_type == "Suspect Sign Smell":
-                values = ast.literal_eval(datasmells['Faulty Element Overview'].values[0])
-                min_value = dataset.loc[~dataset[attribute].isin(values)][attribute].min()
+                values = ast.literal_eval(datasmells[datasmells["attribute"] == attribute]['Faulty Element Overview'].values[0])
                 max_value = dataset.loc[~dataset[attribute].isin(values)][attribute].max()
+                min_value = dataset.loc[~dataset[attribute].isin(values)][attribute].min()
                 dataset[attribute] = dataset[attribute].clip(lower=min_value, upper=max_value)
                 #print(attribute, dataset[attribute].values)
 
-    dataset.to_csv(datasets['path'][i])
+    dataset.to_csv(datasets['path'][i].replace('.csv', '_refactored.csv'))
+    print(datasets['name'][i] + " refactored")
